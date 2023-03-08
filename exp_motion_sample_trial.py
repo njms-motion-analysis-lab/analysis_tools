752af9ee677cf6ccb4647c9321ebfa533476b618
amkostic <amk451@njms.rutgers.edu>
2023-03-08 17:50:52 -0500

    .\exp_motion_sample_trial.py

--- exp_motion_sample_trial.py
+++ exp_motion_sample_trial.py
@@ -1,6 +1,7 @@
 import constants
 import numpy as np
 from typing import Any
+import matplotlib.pyplot as plt
 
 class ExpMotionSampleTrial:
     FILE_PATH = "/Users/stephenmacneille/Documents/labs/NPYViewer/sample_npy_files/filename.npy"
@@ -11,6 +12,32 @@ class ExpMotionSampleTrial:
         self.gradients = measurements[constants.GRADIENTS]
         self.positional = measurements[constants.POSITIONAL]       
         self.sub_motions = list(self.split_series())
+        self.valid_sub_motions = self.get_valid_sub_motions()
+        self.normalized_sub_motions = list(self.normalize())
+
+
+    def get_valid_sub_motions(self):
+        from motion_filter import MotionFilter
+        return MotionFilter.get_valid_motions(self)
+
+    def normalize(self):
+        #normalize amplitude
+        normed = []
+        for motion in self.valid_sub_motions:
+            normed_amplitude = abs(motion/np.max(np.abs(motion)))
+            start, end = normed_amplitude.index[0], normed_amplitude.index[-1]
+            x_vals = np.arange(start,end,(end-start)/100).tolist()
+            normed_temporally = np.interp(x_vals, normed_amplitude.index.tolist(), normed_amplitude)
+            #plt.plot(motion, label="orig")
+            #plt.plot(normed_temporally, label = "normed")
+            #plt.legend()
+            #plt.show()
+
+            normed.append(normed_temporally)
+
+        return normed
+
+
 
     # Splits the series based on zero value crossing.
     def split_series(self) -> Any:
@@ -26,6 +53,9 @@ class ExpMotionSampleTrial:
             start = end
 
         return series[start:]
+
+    #def normalize(self) -> 
+
     
     # Gets the other axes for a provided coordinate.
     def letter_substitution(self) -> Any:
